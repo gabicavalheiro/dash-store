365b670f4a93367876160d7d32d71eb708512135
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-03-01 14:51:53 -0300

    ajuste nos cards de nossos produtos

--- src/components/productsSection.jsx
+++ src/components/productsSection.jsx
@@ -1,100 +1,101 @@
-import './productsSection.css'
+import './productsSection.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import React, { useEffect, useState } from 'react';
 import client from '../sanity.mjs';
 import useSanityImage from '../hooks/useSanityImage';
 
 
+const getImageUrlComplete = async (card, urlFor) => {
+    const { imagem } = card;
 
-async function fetchData() {
-    try {
-        const query = `*[_type == "productsCatalog"][0]`;
-        const Data = await client.fetch(query);
-        console.log('Dados do:',Data); // Log dos dados do cabeçalho
-        return Data;
-    } catch (error) {
-        console.error('Erro ao buscar dados no Sanity:', error);
-        return null;
+    const imageUrl = imagem?.asset?._ref ? urlFor(imagem.asset._ref) : null;
+    let imageUrlComplete = null;
+    if (imageUrl && imageUrl.options && imageUrl.options.baseUrl && imageUrl.options.source) {
+        const { baseUrl, projectId, dataset, source } = imageUrl.options;
+        const imageName = source.substring(6);
+        const formattedImageName = imageName.replace('-png', '.png');
+        imageUrlComplete = `${baseUrl}/images/${projectId}/${dataset}/${formattedImageName}`;
     }
-}
-export default function ProductsSection() {
 
-    const [Data, setData] = useState(null);
+    return imageUrlComplete;
+};
 
-    useEffect(() => {
-        fetchData().then(Data => {
-            setData(Data);
-        });
-    }, []);
-
+const useFetchCardDataAndRender = () => {
+    const [cardData, setCardData] = useState(null);
     const urlFor = useSanityImage();
 
+    useEffect(() => {
+        const fetchCardData = async () => {
+            try {
+                const query = `*[_type == "productsCatalog"][0]`;
+                const response = await client.fetch(query);
 
+                if (response && Array.isArray(response.cards)) {
+                    const allCards = response.cards.flatMap(array => array);
+                    const cardImageData = await Promise.all(allCards.map(async (card) => {
+                        const imageUrlComplete = await getImageUrlComplete(card, urlFor);
+                        return { ...card, imageUrlComplete };
+                    }));
+
+                    setCardData(cardImageData);
+
+                } else {
+                    console.error('A resposta não contém dados válidos.');
+                }
+            } catch (error) {
+                console.error('Erro ao buscar dados do card no Sanity:', error);
+            }
+        };
+
+        fetchCardData();
+    }, [urlFor]);
+
+    return cardData;
+};
+
+export default function ProductsSection() {
+    const cardData = useFetchCardDataAndRender();
+
+    // Limitando o número de cards renderizados a 4
+    const limitedCardData = cardData ? cardData.slice(0, 6) : [];
+
+    // Função para dividir o array em pedaços de tamanho 2
+    function chunkArray(arr, chunkSize) {
+        const chunks = [];
+        for (let i = 0; i < arr.length; i += chunkSize) {
+            chunks.push(arr.slice(i, i + chunkSize));
+        }
+        return chunks;
+    }
 
     return (
-        (Data &&
         <div className="section">
             <div className="titulo">
                 NOSSOS PRODUTOS
             </div>
 
-            <div className="cards">
-                <div className="card">
-                    <div className="produto">
-                        <h1>{Data.titulo1}</h1>
-                    </div>
-                    <div className="imagem">
-                    {urlFor && Data.imagem1 && <img src={urlFor(Data.imagem1)} alt={Data.alt1} width="200" height="230" />}
-                    </div>
-                    <div className="seta">
-                        <i class="bi bi-arrow-right"><a href={Data.link1}></a></i>
-                    </div>
+            {chunkArray(limitedCardData, 2).map((row, rowIndex) => (
+                <div className="cards" key={rowIndex}>
+                    {row.map((product, index) => (
+                        <div className="card" key={index}>
+                            <div className="produto">
+                                <h1>{product.titulo}</h1>
+                            </div>
+                            <div className="imagem">
+                                <img src={product.imageUrlComplete} alt={product.titulo} width="200" height="230" />
+                            </div>
+                            <div className="seta">
+                                <i className="bi bi-arrow-right"><a href={product.link}></a></i>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div className="card">
-                    <div className="produto">
-                        <h1>{Data.titulo2}</h1>
-                    </div>
-                    <div className="imagem">
-                    {urlFor && Data.imagem2 && <img src={urlFor(Data.imagem2)} alt={Data.alt2} width="200" height="230" />}
-                    </div>
-                    <div className="seta">
-                    <i class="bi bi-arrow-right"><a href={Data.link2}></a></i>
-                    </div>
-                </div>
-            </div>
+            ))}
 
-            <div className="cards">
-                <div className="card">
-                    <div className="produto">
-                        <h1>{Data.titulo3}</h1>
-                    </div>
-                    <div className="imagem">
-                    {urlFor && Data.imagem3 && <img src={urlFor(Data.imagem3)} alt={Data.alt3} width="200" height="230" />}
-                    </div>
-                    <div className="seta">
-                    <i class="bi bi-arrow-right"><a href={Data.link3}></a></i>
-                    </div>
-
-                </div>
-                <div className="card">
-                    <div className="produto">
-                        <h1>{Data.titulo4}</h1>
-                    </div>
-                    <div className="imagem">
-                    {urlFor && Data.imagem4 && <img src={urlFor(Data.imagem4)} alt={Data.alt4} width="200" height="230" />}
-                    </div>
-                    <div className="seta">
-                    <i class="bi bi-arrow-right"><a href={Data.link4}></a></i>
-                    </div>
-
-                </div>
-            </div>
             <div className="btn">
                 <button>Ver mais</button>
             </div>
         </div>
-    ))
-
+    )
 }
--- src/dash-store-backend/schemaTypes/products.js
+++ src/dash-store-backend/schemaTypes/products.js
@@ -2,86 +2,41 @@ export default {
     name: 'productsCatalog',
     type: 'document',
     title: 'Seções de produtos',
+
     fields: [
       {
-        name: 'titulo1',
-        type: 'string',
-        title: 'Titulo Card 1'
-      },
-      {
-        name: 'imagem1',
-        type: 'image',
-        title: 'Imagem Card 1'
-      },
-      {
-        name: 'link1',
-        type: 'text',
-        title: 'link Card 1'
-      },
-      {
-        name: 'alt1',
-        type: 'string',
-        title: 'titulo da imagem Card 1'
+          name: 'cards',
+          title: 'Cards',
+          type: 'array',
+          description: 'Serão exibidos na página apenas 6 cards.',
+          of: [
+              {
+                  type: 'object',
+                  fields: [
+                   { name: 'titulo',
+                    type: 'string',
+                    title: 'Titulo'
+                  },
+                  {
+                    name: 'imagem',
+                    type: 'image',
+                    title: 'Imagem',
+                    description: 'A imagem precisa estar em .png com o fundo removido'
+                  },
+                  {
+                    name: 'link',
+                    type: 'text',
+                    title: 'link'
+                  },
+                  {
+                    name: 'alt',
+                    type: 'string',
+                    title: 'titulo da imagem'
+                  }
+                  
+                  ] 
+
+                }]}]}
+
+    
       
-      },
-      {
-        name: 'titulo2',
-        type: 'string',
-        title: 'Titulo Card 2'
-      },
-      {
-        name: 'imagem2',
-        type: 'image',
-        title: 'Imagem Card 2'
-      },
-      {
-        name: 'link2',
-        type: 'text',
-        title: 'link Card 2'
-      },
-      {
-        name: 'alt2',
-        type: 'string',
-        title: 'titulo da imagem Card 2'
-      },
-      {
-        name: 'titulo3',
-        type: 'string',
-        title: 'Titulo Card 3'
-      },
-      {
-        name: 'imagem3',
-        type: 'image',
-        title: 'Imagem Card 3'
-      },
-      {
-        name: 'link3',
-        type: 'text',
-        title: 'link Card 3'
-      },
-      {
-        name: 'alt3',
-        type: 'string',
-        title: 'titulo da imagem Card 3'
-      },
-      {
-        name: 'titulo4',
-        type: 'string',
-        title: 'Titulo Card 4'
-      },
-      {
-        name: 'imagem4',
-        type: 'image',
-        title: 'Imagem Card 4'
-      },
-      {
-        name: 'link4',
-        type: 'text',
-        title: 'link Card 4'
-      },
-      {
-        name: 'alt4',
-        type: 'string',
-        title: 'titulo da imagem Card 4'
-      },
-    ]}
