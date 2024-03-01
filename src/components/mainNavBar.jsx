8d09d98e252af611cfbf4247428214b0c25c4457
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-03-01 15:30:57 -0300

    MainNavBar responsive

--- src/components/mainNavBar.jsx
+++ src/components/mainNavBar.jsx
@@ -6,11 +6,11 @@ import './mainNavBar.css';
 
 export default function MainNavBar() {
     return (
-       <>
+        <>
             <div className="cabecalho">
                 <div className="logo">
                     <a href="#">
-                    <img src="./logoAzul.png" alt="logo" className="titulo" />
+                        <img src="./logoAzul.png" alt="logo" className="titulo" />
                     </a>
                 </div>
 
@@ -31,6 +31,41 @@ export default function MainNavBar() {
             </div>
 
 
+
+            <div className="cabecalho-mobile">
+                <div className="logo-mobile">
+                    <a href="#"></a>
+                    <img src="./icone-dash-branco.png" alt="" width="70" height="70" />
+                </div>
+
+                <div className="head-mobile">
+                    <nav className="navbar"> {/* Certifique-se de que a classe "navbar" está aplicada aqui */}
+                        <div className="nav-mobile">
+                            <div class="dropdown">
+                                <button class="btn dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-list">    </i>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Início</a></li>
+                                    <li><a class="dropdown-item" href="#">Destaques</a></li>
+                                    <li><a class="dropdown-item" href="#">Produtos</a></li>
+                                    <li><a class="dropdown-item" href="#">Contato</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </nav>
+                </div>
+
+            </div>
+
+
         </>
     );
 }
+
+
+
+
+
+
