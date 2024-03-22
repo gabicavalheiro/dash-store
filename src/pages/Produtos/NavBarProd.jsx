78b807c869cd04bcb8ba5658424966ffd2670514
gabicavalheiro <85797302+gabicavalheiro@users.noreply.github.com>
2024-03-22 09:30:51 -0300

    pronto!!

--- src/pages/Produtos/NavBarProd.jsx
+++ src/pages/Produtos/NavBarProd.jsx
@@ -3,21 +3,23 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import React, { useRef } from 'react';
 import './nav.css';
-import { Dropdown, Button } from 'react-bootstrap'; // Importe os componentes do react-bootstrap
+import { Dropdown, Button } from 'react-bootstrap';
 
 export default function NavBar() {
   const mainRef = useRef(null);
 
-  // Obtenha o caminho base da imagem com base na URL atual
-  const getLogoImagePath = () => {
-    const basePath = process.env.PUBLIC_URL; // Obtenha o caminho base do diretório public
-    const currentPath = window.location.pathname; // Obtenha a URL atual da página
+  const getLogoImagePath = (isMobile) => {
+    const basePath = process.env.PUBLIC_URL;
+    const currentPath = window.location.pathname;
 
-    // Determine o caminho da imagem com base na URL atual
-    if (currentPath === '/produtos/categoria') {
-      return `${basePath}/logoAzul.png`; // Caminho da imagem para a rota '/produtos/categoria'
+    if (currentPath === '/produtos/categoria' || currentPath === '/produtos') {
+      if (isMobile) {
+        return `${basePath}/icone_dashAzul.png`; // Caminho da imagem para o cabeçalho móvel
+      } else {
+        return `${basePath}/logoAzul.png`; // Caminho da imagem para o cabeçalho normal
+      }
     } else {
-      return `${basePath}/logoDefault.png`; // Caminho da imagem padrão para outras rotas
+      return `${basePath}/icone_dashAzul.png`; // Caminho da imagem padrão para outras rotas
     }
   };
 
@@ -26,26 +28,25 @@ export default function NavBar() {
       <div className="cabecalho-prod" id='main' ref={mainRef}>
         <div className="logo-prod">
           <a href="/">
-            <img src={getLogoImagePath()} alt="logo" className="titulo" />
+            <img src={getLogoImagePath(false)} alt="logo" className="titulo" style={{ maxWidth: '836px' }} />
           </a>
         </div>
 
         <div className="head-prod">
-          <nav className="navbar"> {/* Certifique-se de que a classe "navbar" está aplicada aqui */}
+          <nav className="navbar">
             <a href="/"><i className="bi bi-arrow-left-circle-fill"></i></a>
           </nav>
         </div>
-
       </div>
 
       <div className="cabecalho-mobile-prod">
         <div className="logo-mobile-prod">
           <a href="/"></a>
-          <img src={getLogoImagePath()} alt="" width="70" height="70" />
+          <img src={getLogoImagePath(true)} alt="" width="70" height="70" style={{ maxWidth: '836px' }} />
         </div>
 
         <div className="head-mobile-prod">
-          <nav className="navbar"> {/* Certifique-se de que a classe "navbar" está aplicada aqui */}
+          <nav className="navbar">
             <a href="/"><i className="bi bi-arrow-left-circle-fill"></i></a>
           </nav>
         </div>
